36b0b51774a0e907c6d53978a9a4db3ee1a0c051
dipakn491 <dipakn491@gmail.com>
2024-03-22 21:36:26 +0530

    first day

---
+++ Descending.java
@@ -0,0 +1,15 @@
+public class Descending {
+
+    public static void DescendingLoop() {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DescendingLoop();
+
+    }
+
+}
---
+++ Sum.java
@@ -0,0 +1,16 @@
+
+public class Sum {
+
+    public static void SumofNumber() {
+        int sum = 0;
+        for (int i = 1; i <= 15; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        SumofNumber();
+    }
+
+}
